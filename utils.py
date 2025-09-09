070d6c52160c49481f82052d7ffb5e6a51222c24
Vinicius Guerra <itsviniciusguerra@gmail.com>
2025-09-09 15:30:27 -0300

    feat: added convolutional layer

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from collections import deque
 import torch.nn as nn
 import numpy as np
 import random
+import torch
 
 class QNetwork(nn.Module):
     def __init__(self, state_dim, action_dim, hidden_dim=128):
@@ -15,6 +16,33 @@ class QNetwork(nn.Module):
             nn.Linear(hidden_dim, action_dim)
         )
     def forward(self, x):
+        return self.net(x)
+
+class ConvolutionalQNetwork(nn.Module):
+    def __init__(self, input_shape, action_dim, hidden_dim=128):
+        super().__init__()
+        self.convNet = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.net = nn.Sequential(
+            nn.Linear(self._get_conv_output(input_shape), hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, action_dim)
+        )
+
+    def _get_conv_output(self, shape):
+        dummy_input = torch.zeros(1, *shape)
+        dummy_output = self.convNet(dummy_input)
+        return int(torch.prod(torch.tensor(dummy_output.size())))
+
+    def forward(self, x):
+        x = self.convNet(x).view(x.size()[0], -1)
         return self.net(x)
 
 class ReplayBuffer:
